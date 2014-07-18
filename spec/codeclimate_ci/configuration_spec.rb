e3619068a2f2bca52e62c8c7694c82459e8fdb1b
ildarsafin <ildar.safin@flatstack.com>
2014-07-18 12:31:31 +0400

    Add spec for Configuration class

---
+++ spec/codeclimate_ci/configuration_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+module CodeclimateCi
+  describe Configuration do
+    let(:options) do
+      {
+        'codeclimate_api_token' => '12345678',
+        'repo_id' => '124356',
+        'branch_name' => 'new-feature'
+      }
+    end
+
+    let(:configuration) { CodeclimateCi::Configuration.new }
+
+    describe '#load_from_options(options)' do
+      before do
+        configuration.load_from_options(options)
+      end
+
+      it 'properly setups options' do
+        expect(configuration.codeclimate_api_token).to eq('12345678')
+        expect(configuration.repo_id).to eq('124356')
+        expect(configuration.branch_name).to eq('new-feature')
+      end
+    end
+  end
+end
