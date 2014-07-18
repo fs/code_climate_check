b5fae5af44b87636b6c57486d0bf11b2d61a92c4
ildarsafin <ildar.safin@flatstack.com>
2014-07-18 12:30:56 +0400

    Require new Cli and Configuration classes

--- lib/codeclimate_ci.rb
+++ lib/codeclimate_ci.rb
@@ -1,5 +1,10 @@
 require 'codeclimate_ci/version'
+require 'codeclimate_ci/cli'
+require 'codeclimate_ci/configuration'
 require 'codeclimate_ci/compare_gpa'
 
 module CodeclimateCi
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
 end
