5def047d49c0e41db52c4b26dfd4043143c7f94f
ildarsafin <ildar.safin@flatstack.com>
2014-07-21 13:28:28 +0400

    Fix compare_gpa and get_gpa test specs

--- spec/codeclimate_ci/compare_gpa_spec.rb
+++ spec/codeclimate_ci/compare_gpa_spec.rb
@@ -5,7 +5,7 @@ module CodeclimateCi
     let(:codeclimate_ci) { CodeclimateCi::CompareGpa.new('repo11', 'token1') }
 
     before do
-      allow_any_instance_of(GetGpa).to receive(:gpa_for).and_return(1)
+      allow_any_instance_of(GetGpa).to receive(:gpa).and_return(1)
     end
 
     it 'should not be worse' do
--- spec/codeclimate_ci/get_gpa_spec.rb
+++ spec/codeclimate_ci/get_gpa_spec.rb
@@ -2,16 +2,18 @@ require 'spec_helper'
 
 module CodeclimateCi
   describe GetGpa do
-    let(:codeclimate_ci) { CodeclimateCi::GetGpa.new('repo11', 'token1') }
+    let(:codeclimate_ci) { CodeclimateCi::GetGpa.new(api_requester, 'repo1') }
+    let(:api_requester) { double }
 
     before do
-      allow(Net::HTTP).to receive(:get_response)
-      allow(Net::HTTP.get_response).to receive(:is_a?).and_return(true)
-      allow(Net::HTTP.get_response).to receive(:body).and_return({ last_snapshot: { gpa: 3 } }.to_json)
+      allow(api_requester).to receive(:branch_info)
+        .and_return(
+          'last_snapshot' => { 'gpa' => 3 }
+        )
     end
 
     it 'should return correct gpa value' do
-      expect(codeclimate_ci.gpa_for('master')).to eq 3
+      expect(codeclimate_ci.gpa).to eq 3
     end
   end
 end
