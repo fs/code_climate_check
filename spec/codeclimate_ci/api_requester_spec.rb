3aa4da26d00b798eb9633ed211dcfd4926a91691
ildarsafin <ildar.safin@flatstack.com>
2014-07-24 17:53:38 +0400

    Refactor api_requester spec

--- spec/codeclimate_ci/api_requester_spec.rb
+++ spec/codeclimate_ci/api_requester_spec.rb
@@ -4,16 +4,17 @@ module CodeclimateCi
   describe ApiRequester do
     let(:token) { 'abc123' }
     let(:repo_id) { '12345' }
-    let(:api_requester) { CodeclimateCi::ApiRequester.new(token, repo_id) }
-
-    before do
-      allow(ApiRequester).to receive(:get)
+    let(:branch_name) { 'awesome_branch' }
+    let(:api_requester) { ApiRequester.new(token, repo_id) }
+    let(:request_to_codeclimate) { api_requester.branch_info(branch_name) }
+    let(:url_to_codeclimate) do
+      "https://codeclimate.com/api/repos/#{repo_id}/branches/#{branch_name}?api_token=#{token}"
     end
 
-    it 'should receive Get method' do
-      expect(ApiRequester).to receive(:get)
+    before { stub_request(:get, url_to_codeclimate).to_return(status: 200) }
 
-      api_requester.branch_info('hello')
+    it 'should request to url correctly' do
+      expect(request_to_codeclimate.code).to eq 200
     end
   end
 end
