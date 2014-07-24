9ff3a839f63796707d5b0808d6683dbdc4f6dde3
ildarsafin <ildar.safin@flatstack.com>
2014-07-24 17:50:35 +0400

    Replace first base_url by constant variable in api_requester file

--- lib/codeclimate_ci/api_requester.rb
+++ lib/codeclimate_ci/api_requester.rb
@@ -3,10 +3,10 @@ require 'httparty'
 module CodeclimateCi
   class ApiRequester
     include HTTParty
-    base_uri 'https://codeclimate.com/api/repos'
+    CODECLIMATE_URI =  'https://codeclimate.com/api/repos'
 
     def initialize(token, repo_id)
-      self.class.base_uri "#{self.class.base_uri}/#{repo_id}"
+      self.class.base_uri "#{CODECLIMATE_URI}/#{repo_id}"
       self.class.default_params api_token: token
     end
 
