1f32ef0de22465a69b5db7a96dcf6d3a449eec6f
ildarsafin <ildar.safin@flatstack.com>
2014-08-01 16:35:32 +0400

    Fix spec in compare gpa spec

--- spec/codeclimate_ci/compare_gpa_spec.rb
+++ spec/codeclimate_ci/compare_gpa_spec.rb
@@ -5,17 +5,20 @@ module CodeclimateCi
     let(:codeclimate_ci) { CodeclimateCi::CompareGpa.new('repo11', 'token1') }
 
     before do
-      allow_any_instance_of(ApiRequester).to receive(:branch_info)
+      allow_any_instance_of(ApiRequester).to receive(:branch_info).with('master')
         .and_return(
           'last_snapshot' => { 'gpa' => 3 }
+        )
+
+      allow_any_instance_of(ApiRequester).to receive(:branch_info).with('another_branch')
+        .and_return(
+          'last_snapshot' => { 'gpa' => 2 }
         )
     end
 
     context 'when branch is analyzed' do
-      before { allow_any_instance_of(GetGpa).to receive(:gpa).and_return(1) }
-
       it 'should not be worse' do
-        expect(codeclimate_ci.worse?('master')).to be_falsey
+        expect(codeclimate_ci.worse?('another_branch')).to be_truthy
       end
     end
   end
