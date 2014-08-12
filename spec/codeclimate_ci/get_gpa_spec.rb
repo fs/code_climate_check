64c193d5ccb651cbb5198718247a7c728743a6de
ildarsafin <ildar.safin@flatstack.com>
2014-08-12 13:38:37 +0400

    Add spec for invalid credentials handling in get_gpa_spec

--- spec/codeclimate_ci/get_gpa_spec.rb
+++ spec/codeclimate_ci/get_gpa_spec.rb
@@ -12,6 +12,7 @@ describe CodeclimateCi::GetGpa do
   context 'when task is analyzed' do
     before do
       allow(api_requester).to receive(:branch_info) { analyzed_branch_info }
+      allow(api_requester.branch_info).to receive(:code).and_return(200)
     end
 
     it 'returns correct gpa value' do
@@ -26,6 +27,7 @@ describe CodeclimateCi::GetGpa do
 
     before do
       allow(api_requester).to receive(:branch_info) { empty_branch_info }
+      allow(api_requester.branch_info).to receive(:code).and_return(200)
     end
 
     it 'returns correct gpa value' do
@@ -44,7 +46,33 @@ describe CodeclimateCi::GetGpa do
 
     it 'returns correct gpa value after retry' do
       expect(api_requester).to receive(:branch_info).twice
+      allow(api_requester.branch_info).to receive(:code).and_return(200)
       expect(codeclimate_ci.gpa).to eq 3
+    end
+  end
+
+  context 'when invalid credentials given' do
+    let(:configuration) do
+      double(CodeclimateCi::Configuration, retry_count: '2', sleep_time: '0')
+    end
+
+    before do
+      allow(api_requester).to receive(:branch_info) { empty_branch_info }
+      allow(api_requester.branch_info).to receive(:code).and_return(404)
+      allow(CodeclimateCi::Report).to receive(:invalid_credentials)
+      allow(codeclimate_ci).to receive(:exit)
+    end
+
+    it 'reports about invalid_credentials' do
+      expect(CodeclimateCi::Report).to receive(:invalid_credentials)
+
+      codeclimate_ci.gpa
+    end
+
+    it 'exites from script' do
+      expect(codeclimate_ci).to receive(:exit)
+
+      codeclimate_ci.gpa
     end
   end
 end
