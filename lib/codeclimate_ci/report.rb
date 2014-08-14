5dca760fbda5badeb9052733b22233ec23d02b75
ildarsafin <ildar.safin@flatstack.com>
2014-08-14 15:47:01 +0400

    Replace Report class by module

--- lib/codeclimate_ci/report.rb
+++ lib/codeclimate_ci/report.rb
@@ -1,22 +1,24 @@
 module CodeclimateCi
-  class Report
-    def self.result_not_ready(retry_count, branch)
+  module Report
+    module_function
+
+    def result_not_ready(retry_count, branch)
       puts "Retry #{retry_count}. #{branch} branch analyze is not ready or branch does not exist"
     end
 
-    def self.worse_code(gpa_diff)
+    def worse_code(gpa_diff)
       puts "Code in your branch became worse on #{rounded_diff_value(gpa_diff)} points"
     end
 
-    def self.good_code(gpa_diff)
+    def good_code(gpa_diff)
       puts "Gpa score has improved to #{rounded_diff_value(gpa_diff)} points. Go on..."
     end
 
-    def self.invalid_credentials
-      puts 'Given invalid credentials. Please check your codeclimate_api_token and repo_id.'
+    def invalid_credentials
+      puts 'Invalid credentials given. Please check your codeclimate_api_token and repo_id.'
     end
 
-    def self.rounded_diff_value(gpa_diff)
+    def rounded_diff_value(gpa_diff)
       gpa_diff.abs.round(2)
     end
   end
