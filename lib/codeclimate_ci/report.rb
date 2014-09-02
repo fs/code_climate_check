27d89aabc91fcc183ee5a61c6ba9db17434b562f
Timur Vafin <timur.vafin@flatstack.com>
2014-09-02 17:51:16 +0400

    Remove url_to_branch method

--- lib/codeclimate_ci/report.rb
+++ lib/codeclimate_ci/report.rb
@@ -3,32 +3,28 @@ module CodeclimateCi
     module_function
 
     def result_not_ready(retry_count, branch)
-      puts "Retry #{retry_count}. #{branch} branch analyze is not ready or branch does not exist"
+      puts "Retry #{retry_count}. #{branch} branch analyze is not ready or branch does not exist."
     end
 
     def worse_code(gpa_diff)
-      puts "Code in your branch became worse on #{rounded_diff_value(gpa_diff)} points"
-
-      url_to_branch
+      puts "Code in your branch became worse on #{rounded_diff_value(gpa_diff)} points."
+      puts "https://codeclimate.com/repos/#{configuration.repo_id}/compare/#{configuration.branch_name}"
     end
 
     def good_code(gpa_diff)
-      puts "Gpa score has improved to #{rounded_diff_value(gpa_diff)} points. Go on..."
+      puts "Gpa score has improved to #{rounded_diff_value(gpa_diff)} points."
     end
 
     def invalid_credentials
       puts 'Invalid credentials given. Please check your codeclimate_api_token and repo_id.'
     end
 
-    def url_to_branch
-      repo_id = CodeclimateCi.configuration.repo_id
-      branch_name = CodeclimateCi.configuration.branch_name
-
-      puts "https://codeclimate.com/repos/#{repo_id}/compare/#{branch_name}"
-    end
-
     def rounded_diff_value(gpa_diff)
       gpa_diff.abs.round(2)
+    end
+
+    def configuration
+      CodeclimateCi.configuration
     end
   end
 end
